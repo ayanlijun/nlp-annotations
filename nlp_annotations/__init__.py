3f7834218b6381730fa70136906347c8ad95bab5
asciialex <alex@asciialex.com>
2020-03-23 16:04:46 +0800

    Remove link extraction to a seperate generator for better modularity

--- nlp_annotations/__init__.py
+++ nlp_annotations/__init__.py
@@ -5,20 +5,24 @@ import re
 RP = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
 
 
+def markdown_string_link_generator(markdown_string: str) -> ty.Generator:
+    for link in RP.finditer(markdown_string):
+        yield type(
+            "link", (), {"word": link.groups()[0], "entity": link.groups()[1], "start": link.start(0), "end": link.end(0)})
+
+
 def markdown_links2entity_list(markdown_string: str) -> ty.Tuple:
     linkless_string = ""
     entities = []
     idx = 0
-    for link in RP.finditer(markdown_string):
-        word, entity = link.groups()[0], link.groups()[1]
-        start, end = link.start(0), link.end(0)
-        linkless_string += markdown_string[idx:start]
+    for link in markdown_string_link_generator(markdown_string):
+        linkless_string += markdown_string[idx:link.start]
         word_idx = len(linkless_string)
-        linkless_string += word
-        idx = end
-        entities.append((word_idx, word_idx + len(word), entity))  # Append to entities
+        linkless_string += link.word
+        idx = link.end
+        entities.append((word_idx, word_idx + len(link.word), link.entity))  # Append to entities
 
-    linkless_string += markdown_string[end:]  # Add the rest of the string
+    linkless_string += markdown_string[link.end:]  # Add the rest of the string
     return (linkless_string, {"entities": entities})
 
 
